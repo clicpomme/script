a586fe1efc34d9ff1076c28a8274a4b5409470eb
clicpomme <rich@Rich-ClicPommedotcom.local>
2015-06-03 11:02:58 -0400

    update for jamf Extension attribute

--- UserHomeSize.sh
+++ UserHomeSize.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # Richard Charbonneau Clicpomme 2015
 UserList=`dscl . list /Users UniqueID | awk '$2 > 501 { print $1 }'` 
 
@@ -8,6 +8,7 @@ if [ -d /Users/$u/ ]; then
 results=`du -sh /Users/$u/ | grep -v \ | grep -v "\.plist" | awk '{print $1}'`
 else results= "N/A"
 fi
-echo $u $results
+echo "<result> $u $results </result>"
 done
+
 
