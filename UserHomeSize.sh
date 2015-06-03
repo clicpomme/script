3ee89c91f287049cd17e7d440b573b0fae359583
clicpomme <rich@Rich-ClicPommedotcom.local>
2015-06-03 10:18:57 -0400

    remove result

--- UserHomeSize.sh
+++ UserHomeSize.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 # Richard Charbonneau Clicpomme 2015
-echo "<result>"
-
 UserList=`dscl . list /Users UniqueID | awk '$2 > 501 { print $1 }'` 
 
 for u in $UserList ; 
 do
 if [ -d /Users/$u/ ]; then
 results=`du -sh /Users/$u/ | grep -v \ | grep -v "\.plist" | awk '{print $1}'`
-else continue
+else results= "N/A"
 fi
 echo $u $results
-
 done
-echo "</result>"
-exit 0
+
